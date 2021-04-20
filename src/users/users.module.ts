f78900c5b9a0c57570a9e9723b3f6fa624ee56c8
dinhpv <pvd02091992@gmaill.com>
2021-04-20 12:18:10 +0700

    add middleware

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
+import {
+  Module,
+  NestModule,
+  MiddlewareConsumer,
+  RequestMethod,
+} from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersCreateMiddleware } from './middlewares/users.middleware';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(UsersCreateMiddleware)
+      .forRoutes({ path: '/users', method: RequestMethod.POST });
+  }
+}
